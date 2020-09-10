05ceca3c10fa93d7ed5f170f77a12b1193b71c94
Ian Andrewson <ianandrewson@gmail.com>
2020-09-10 12:25:12 -0700

    added brief notes on functionality to Player and Library.

--- src/components/library/Library.jsx
+++ src/components/library/Library.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 export default function Library() {
-  
+
 
   return (
     <>
       <h1>This is the Libray!</h1>
+      <p>It should make a fetch to show the contents of the users library</p>
+      <p>It should display the library in a table</p>
+      <p>The table should have a play button to fetch the stream</p>
     </>
   );
 }
--- src/components/player/Player.jsx
+++ src/components/player/Player.jsx
@@ -4,7 +4,9 @@ export default function Player() {
 
   return (
     <>
-      <h1>THIS IS THE PLAYER PAGE YEY!</h1>
+      <h1>THIS IS THE PLAYER COMPONENT YEY!</h1>
+      <p>It sould load the stream, whatever the hell that means</p>
+      <p>It sould have playback controls</p>
       <audio src=''>
         Your browser does not support an audio player.
       </audio>
